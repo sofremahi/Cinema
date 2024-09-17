f8592cde54519620de05155a5694b6da7065766d
fatehian.m <fatehian.m@tiddev.com>
2024-09-17 09:11:32 +0330

    Merge remote-tracking branch 'origin/upgraded' into upgraded

--- src/main/java/com/tiddev/cinema/service/modelDto/CinemaInfoDto.java
+++ src/main/java/com/tiddev/cinema/service/modelDto/CinemaInfoDto.java
@@ -1,8 +1,6 @@
 package com.tiddev.cinema.service.modelDto;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
 
 import java.util.Map;
 
